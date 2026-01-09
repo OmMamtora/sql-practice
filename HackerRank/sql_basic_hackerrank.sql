03e9c6bc0b2059d3e42d7ff7e9cd70bb9c9ff55e
Om Mamtora <94129978+OmMamtora@users.noreply.github.com>
2026-01-09 22:43:20 +0530

    Add SQL queries for various data retrieval tasks

--- HackerRank/sql_basic_hackerrank.sql
+++ HackerRank/sql_basic_hackerrank.sql
@@ -72,3 +72,35 @@ select distinct city from station where (city like 'A%'
                                     OR city like '%i'
                                     OR city like '%o'
                                     OR city like '%u');
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[^AEIOU]' OR CITY REGEXP '[^AEIOU]$';
+
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION  WHERE CITY REGEXP '^[^AEIOU]' AND CITY REGEXP '[^AEIOU]$'; 
+
+
+-- Query the Name of any student in STUDENTS who scored higher than Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+select name from students where marks > 75 order by right (name, 3),ID ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+select name from employee order by name ASC;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than months. Sort your result by ascending employee_id.
+select name from employee where salary > 2000 AND months < 10 order by employee_id ASC;
+
+-- Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+	-- Equilateral: It's a triangle with  sides of equal length.
+	-- Isosceles: It's a triangle with sides of equal length.
+	-- Scalene: It's a triangle with sides of differing lengths.
+	-- Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+select 
+    case
+        when A + B <= C or A + C <= B or B + C <= A THEN 'Not A Triangle'
+        when A = B AND B = C THEN 'Equilateral'
+        when A = B or B = C or C = A THEN 'Isosceles'
+        else 'Scalene'
+    end
+from triangles
